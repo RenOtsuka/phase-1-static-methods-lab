42a0b3d905aba033c8f2225bd25fbbdca23c343f
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-02-14 14:54:56 -0600

    commit tree a82cd3d377057e7e69142f360e0033017c46095d

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 class Formatter {
   //add static methods here
+  static capitalize(str){
+    return str[0].toUpperCase() + str.slice(1);
+  }
+
+  static sanitize(str){
+    return str.replace(/[^a-zA-Z0-9-' ]+/gi, "");
+  }
+
+  static titleize(str){
+    //return str.replace( /\w\S*/g, function(str){
+      //return str.charAt(0).toUpperCase() + str.substr(1).toLowerCase();
+    //})
+    let notCap = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let newS = str.split(" ");
+    //console.log(newS);
+
+    newS[0] = newS[0].slice(0, 1).toUpperCase() + newS[0].slice(1).toLowerCase();
+
+    for (let i = 1; i < newS.length; i++) {
+       if(!notCap.includes(newS[i].toLowerCase())){
+         newS[i] = newS[i][0].toUpperCase() + newS[i].slice(1);
+      }
+      else{
+        newS[i] = newS[i].toLowerCase();
+      }
+    }
+    
+    return newS.join(" ");
+  }
+  
 }
